158a0d7d9f155341e859d7673b3f1b316f4a3ad1
Udhay <udhay1co.de@gmail.com>
2021-05-28 19:03:27 +0530

    first commit

--- Jquery/index.js
+++ Jquery/index.js
@@ -1,1 +1,12 @@
-$("h1").css("color" , "red")
+// what this code does is that it calls for this code once the document rdy
+$(document).ready(function(){
+    $("h1").css("color" , "red")
+
+})
+
+// manipulating Styles in jquery
+
+$("button").css("color" , "Blue");
+
+// Reteriving data with jquery 
+$("h1").css("font-size")
