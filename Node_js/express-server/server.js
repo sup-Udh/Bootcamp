de9c932d625d39f68b34bf34179613c93af03746
Udhay <udhay1co.de@gmail.com>
2021-06-08 16:47:07 +0530

    Seting up the Server.js

---
+++ Node_js/express-server/server.js
@@ -0,0 +1,10 @@
+// Requring the Express Module
+const express = require('express');
+//  Creating the Function for app
+const app = express();
+
+
+app.listen(3000, function(){
+    console.log("😎  ✌ server is running on port 3000 ")
+
+}) ;
