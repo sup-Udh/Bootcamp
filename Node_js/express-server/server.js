11519171aafe10a4247c2b4c0f1f78a140e75ff8
Udhay <udhay1co.de@gmail.com>
2021-06-08 16:58:46 +0530

    server.js

--- Node_js/express-server/server.js
+++ Node_js/express-server/server.js
@@ -3,6 +3,13 @@ const express = require('express');
 //  Creating the Function for app
 const app = express();
 
+// Working on our first route 
+
+app.get("/" , function(request , response)  {
+    console.log(request);
+});
+
+
 
 app.listen(3000, function(){
     console.log("😎  ✌ server is running on port 3000 ")
