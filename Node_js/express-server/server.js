a1461bed5d77fced78d7e247e9f7885fcc7710e3
Udhay <udhay1co.de@gmail.com>
2021-06-08 17:06:36 +0530

    server.js

--- Node_js/express-server/server.js
+++ Node_js/express-server/server.js
@@ -6,10 +6,15 @@ const app = express();
 // Working on our first route 
 
 app.get("/" , function(request , response)  {
-    console.log(request);
+    // console.log(request); 
+
+    response.send("<h1>hello world</h1>");
+
+    // response.send("Welcome to the main page");
+    // We can add html content too 
+  
+
 });
-
-
 
 app.listen(3000, function(){
     console.log("😎  ✌ server is running on port 3000 ")
