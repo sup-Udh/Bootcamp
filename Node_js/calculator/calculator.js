0b0c9fcc9409f2b70bd39994ace37df19892f283
Udhay <udhay1co.de@gmail.com>
2021-06-10 16:28:32 +0530

    Done with the calculator

--- Node_js/calculator/calculator.js
+++ Node_js/calculator/calculator.js
@@ -11,11 +11,18 @@ app.get("/" , function (req,res){
 });
 
 app.post("/" , function(req,res){
-    console.log(req.body);
-    res.send("Thanks for send ur request")
+    // console.log(req.body);
+    // res.send("Thanks for send ur request")
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    // giving the answer to the user while posting the answer in the post page 
+    res.send("the answer to your question is "+ result)
 })
 
 app.listen(3000, function(){
     console.log("😎  ✌ server is running on port 3000 ")
 
-}) ;
+}) ; 
