2de06a42160ed24479e69023351deedcdb29d049
Udhay <udhay1co.de@gmail.com>
2021-06-10 12:45:15 +0530

    test

--- Node_js/calculator/calculator.js
+++ Node_js/calculator/calculator.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.get("/" , function (req,res){
     console.log("user requested for Main page")
-    
+
     res.send("<h1>Welcome to the Main root page</h1>")
 })
+
+app.listen(3000, function(){
+    console.log("😎  ✌ server is running on port 3000 ")
+
+}) ;
