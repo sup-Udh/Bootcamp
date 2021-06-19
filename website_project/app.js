8e7053ece0ded05fb1752f2e5c060de9e5a97378
Udhay <udhay1co.de@gmail.com>
2021-06-19 16:13:45 +0530

    App.js

--- website_project/app.js
+++ website_project/app.js
@@ -6,9 +6,32 @@ app.get("/" ,  function(req, res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=8a81406b375d24c3c3f72ae3fcaa8f5c"
     
     https.get(url, function(response){
-        console.log(response)
+        // console.log(response.statusCode)
+
+        response.on("data",function(data){
+
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+            console.log(temp)
+          
+            // sending to the user 
+            res.send("<h1>he temprature in london is - " + temp + "degress celeisus</h1>.");
+        
+            // console.log(data)
+            // const weather_data = JSON.parse(data)
+            // console.log(weather_data)      
+
+            // // creatoing a test Object 
+            // const object = {
+            //     name : "Udhay",
+            //     Favfood : "Indian"
+            // }
+            // console.log(JSON.stringify(object))
+
+
+          })
     })
-res.send("the server is Running")
+
 })
 
 
