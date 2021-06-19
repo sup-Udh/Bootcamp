ebf7b9b4f218c1a641f6b2901525df0603e3e5ea
Udhay <udhay1co.de@gmail.com>
2021-06-19 13:22:10 +0530

    Api call

--- website_project/app.js
+++ website_project/app.js
@@ -3,7 +3,12 @@ const https = require("https")
 const app = express();
 
 app.get("/" ,  function(req, res){
-    res.send("Welcome to the main  page ")
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=8a81406b375d24c3c3f72ae3fcaa8f5c"
+    
+    https.get(url, function(response){
+        console.log(response)
+    })
+res.send("the server is Running")
 })
 
 
